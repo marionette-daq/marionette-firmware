d7e1e6835e05932feb9f2b0cd372cbe3577ee03d
kwilson <kwilsonpdx@gmail.com>
2014-07-07 14:17:04 -0700

    Changed threads command to be more useful

--- src/marionette/main.c
+++ src/marionette/main.c
@@ -48,27 +48,31 @@ static void cmd_mem(BaseSequentialStream * chp, int argc, char * argv[])
 	chprintf(chp, "heap free total  : %u bytes\r\n", size);
 }
 
-static void cmd_threads(BaseSequentialStream * chp, int argc, char * argv[])
-{
-	static const char * states[] = {THD_STATE_NAMES};
-	Thread * tp;
-	(void)argv;
-	if (argc > 0)
-	{
-		chprintf(chp, "Usage: threads\r\n");
-		return;
-	}
-	chprintf(chp, "    addr    stack prio refs     state time\r\n");
-	tp = chRegFirstThread();
-	do
-	{
-		chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s %lu\r\n",
-		         (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
-		         (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-		         states[tp->p_state], (uint32_t)tp->p_time);
-		tp = chRegNextThread(tp);
-	}
-	while (tp != NULL);
+/*! \brief Show running threads
+ *
+ * @param chp
+ * @param argc
+ * @param argv
+ */
+void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
+    (void)chp;
+    static const char *states[] = {THD_STATE_NAMES};
+    Thread *tp;
+
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: threads\r\n");
+        return;
+    }
+    chprintf(chp, "addr\t\tstack\t\tprio\trefs\tstate\t\ttime\tname\r\n");
+    tp = chRegFirstThread();
+    do {
+        chprintf(chp, "%.8lx\t%.8lx\t%4lu\t%4lu\t%9s\t%lu\t%s\r\n",
+                (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
+                (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
+                states[tp->p_state], (uint32_t)tp->p_time, tp->p_name);
+        tp = chRegNextThread(tp);
+    } while (tp != NULL);
 }
 
 static void cmd_test(BaseSequentialStream * chp, int argc, char * argv[])
