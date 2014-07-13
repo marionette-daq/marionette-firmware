6a8777c8212a4088729835d723e822e6e9f83b7d
kwilson <kwilsonpdx@gmail.com>
2014-07-12 17:28:58 -0700

    Added writer and reader methods as threads

--- library/python/devtest/DUT.py
+++ library/python/devtest/DUT.py
@@ -4,39 +4,69 @@
 """ 
 This tool is used to manipulate a D_evice U_nder T_est
 
-Example Usage:
+Example: 
 
 ~/.../library/python/devtest (python*) > ./DUT.py 
 
 *** INFO:  opened port /dev/ttyACM0
 
---- sent->	 gpio:configure:porth:pin2:output:floating
-
---- sent->	 gpio:set:porth:pin2
-
---- sent->	 gpio:get:porth:pin2
-
---- sent->	 gpio:clear:porth:pin2
-
---- sent->	 gpio:get:porth:pin2
-
---- sent->	 gpio:set:porth:pin2
-
---- sent->	 resetpins
-
---- sent->	 gpio:get:porth:pin2
 
 
-*** INFO:  closed port: /dev/ttyACM0
+m > +noprompt
 
+gpio:configure:porth:pin2:output:floating
+
+        DEBUG:  ../../src/fetch/gpio.c:297:gpio_config()->pin: 2
+
+        DEBUG:  ../../src/fetch/gpio.c:298:gpio_config()->port: 0x40021C00		0x40020000
+
+        DEBUG:  ../../src/fetch/gpio.c:299:gpio_config()->dir: 1
+
+        DEBUG:  ../../src/fetch/gpio.c:300:gpio_config()->sense: 0
+
+gpio:configure:porth:pin2:output:floating
+
+        DEBUG:  ../../src/fetch/gpio.c:297:gpio_config()->pin: 2
+
+        DEBUG:  ../../src/fetch/gpio.c:298:gpio_config()->port: 0x40021C00		0x40020000
+
+        DEBUG:  ../../src/fetch/gpio.c:299:gpio_config()->dir: 1
+
+        DEBUG:  ../../src/fetch/gpio.c:300:gpio_config()->sense: 0
+
+gpio:set:porth:pin2
+
+gpio:get:porth:pin2
+
+1
+
+gpio:clear:porth:pin2
+
+gpio:get:porth:pin2
+
+0
+
+gpio:set:porth:pin2
+
+resetpins
+
+        DEBUG:  ../../src/fetch/fetch.c:155:fetch_resetpins()->Resetting pins
+
+gpio:get:porth:pin2
+
+0
+
++prompt
+
+m > 
 Bye.
-~/.../library/python/devtest (python*) > 
 
 """
 
 import sys
-from time import sleep
+import threading
 import serial
+from time import sleep
 import utils as u
 
 #Exit_Character = serial.to_bytes([0x1d])   # CTRL+]
@@ -57,7 +87,7 @@ class DUTSerial():
         if not self.isOpen:
             self.ser          = serial.Serial()
             self.ser.baudrate = self.baud
-            self.ser.port     = self.serial_port
+            self.ser.port     = self.serial_port   # readline and readlines will block forever if no timeout
             self.ser.timeout  = self.timeout
 
             """
@@ -73,22 +103,87 @@ class DUTSerial():
                 s = "opened port {}\n".format(self.serial_port)
                 u.info(s)
                 self.isOpen       = True
+                self.alive        = True
+                self.alive = True
+                self._start_reader()
+                self.tx_thread = threading.Thread(target=self.writer)
+                self.tx_thread.setDaemon(True)
+                self.tx_thread.start()
+
             else:
                 print("Failed to open port: ", self.serial_port)
                 self.isOpen       = False
+                self.alive        = False
         return
 
-    def read(self):
-        output = self.ser.readline(150) 
-        return output
+    def join(self, tx_only=False):
+        self.tx_thread.join()
+        if not tx_only:
+            self._reader_alive = False
+            self.rx_thread.join()
+
+    def _start_reader(self):
+        """Start reader thread"""
+        self._reader_alive = True
+        self.rx_thread = threading.Thread(target=self.reader)
+        self.rx_thread.setDaemon(True)
+        self.rx_thread.start()
+
+    def reader(self):
+        try:
+            while self.alive and self._reader_alive:
+                line = self.ser.readline(150)    # don't forget timeout setting
+                if len(line) > 0:
+                    #            sys.stdout.write(str(line))
+                    print(line.decode('ascii'))
+                else:
+                    pass
+                # print('*')
+                sys.stdout.flush()
+        except serial.SerialException  as e:
+            self.alive = False
+            raise
+
+    def writer(self):
+        try:
+            if self.alive:
+                self.write("\r\n")
+                sleep(0.500)
+                self.write("+noprompt\r\n")
+                sleep(0.500)
+                self.write("gpio:configure:porth:pin2:output:floating\r\n")
+                sleep(0.500)
+                self.write("gpio:configure:porth:pin2:output:floating\r\n")
+                sleep(0.500)
+                self.write("gpio:set:porth:pin2\r\n")
+                sleep(0.500)
+                self.write("gpio:get:porth:pin2\r\n")
+                sleep(0.500)
+                self.write("gpio:clear:porth:pin2\r\n")
+                sleep(0.500)
+                self.write("gpio:get:porth:pin2\r\n")
+                sleep(0.500)
+                self.write("gpio:set:porth:pin2\r\n")
+                sleep(0.500)
+                self.write("resetpins\r\n")
+                sleep(0.500)
+                self.write("gpio:get:porth:pin2\r\n")
+                sleep(0.500)
+                self.write("+prompt\r\n")
+                sleep(0.500)
+
+        except:
+            self.alive = False
+            raise
 
     def write(self, s):
         bytes = str.encode(s)    # Because python and strings
         numbytes = self.ser.write(bytes) 
-        sys.stdout.write('--- sent->\t ')
-        sys.stdout.write(s)
-        sys.stdout.write('\n')
-        sys.stdout.flush()
+        # console
+        #sys.stdout.write('--- sent->\t ')
+        #sys.stdout.write(s)
+        #sys.stdout.write('\n')
+        #sys.stdout.flush()
         return numbytes
 
     def close(self):
@@ -97,20 +192,10 @@ class DUTSerial():
             s="closed port: {}\n".format(self.serial_port)
             u.info(s)
         self.isOpen = False
+        self.alive  = False
+        self.join(True)
+        self.join()
         return
-
-def DUT_capture(boardout):
-    line = boardout.read()
-    if len(line) > 0:
-        print(line.decode('ascii'))
-    else:
-        print('*')
-    return
-
-def DUT_write(boardout, s):
-    numbytes = boardout.write(s);
-#    print(numbytes)
-    return
 
 if __name__ == "__main__":
     try:
@@ -120,38 +205,18 @@ if __name__ == "__main__":
         timeout        = Default_Timeout
 
         #ser = DUTSerial(baud=baud, timeout=timeout)   # example of default args to init
-        boardout       = DUTSerial(serial_port, baud, timeout)
-        boardout.start()
+        DUT       = DUTSerial(serial_port, baud, timeout)
+        DUT.start()
 
-        DUT_write(boardout, "gpio:configure:porth:pin2:output:floating\r\n")
-        sleep(0.200)
-        DUT_write(boardout, "gpio:set:porth:pin2\r\n")
-        sleep(1.000)
-        DUT_write(boardout, "gpio:get:porth:pin2\r\n")
-        sleep(1.000)
-        DUT_write(boardout, "gpio:clear:porth:pin2\r\n")
-        sleep(1.000)
-        DUT_write(boardout, "gpio:get:porth:pin2\r\n")
-        sleep(1.000)
-        DUT_write(boardout, "gpio:set:porth:pin2\r\n")
-        sleep(1.000)
-        DUT_write(boardout, "resetpins\r\n")
-        sleep(1.000)
-        DUT_write(boardout, "gpio:get:porth:pin2\r\n")
-        sleep(1.000)
-
-        #while True:
-            #DUT_capture(boardout)
-
-        boardout.close()
+        DUT.join()
         print("Bye.")
 
     except serial.SerialException:
-        boardout.close()
+        DUT.close()
         print ('SerialException occurred')
 
     except KeyboardInterrupt:
-        boardout.close()
+        DUT.close()
         print ("\nQuitting")
 
 # Attic
@@ -163,5 +228,4 @@ if __name__ == "__main__":
 #      self.ser.xonxoff  = 1
 #      self.ser.rtscts   = 0
 #      self.ser.dsrdtr   = 0
-
 
