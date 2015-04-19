104b267490baa998bb032c77f95c4c8f7904ee75
SethMWard <setward@pdx.edu>
2015-04-18 22:37:22 -0700

    Added working directory for CAN network

---
+++ src/CAN/.dep/crt0.o.d
@@ -0,0 +1,16 @@
+build/obj/crt0.o: ../../ChibiOS-RT/os/ports/GCC/ARMCMx/crt0.c \
+ /usr/lib/gcc/arm-none-eabi/4.9.2/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h
+
+/usr/lib/gcc/arm-none-eabi/4.9.2/include/stdint.h:
+
+/usr/arm-none-eabi/include/stdint.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
+
+/usr/arm-none-eabi/include/sys/_intsup.h:
---
+++ src/CAN/.dep/mmcsd.o.d
@@ -0,0 +1,217 @@
+build/obj/mmcsd.o: ../../ChibiOS-RT/os/hal/src/mmcsd.c \
+ ../../ChibiOS-RT/os/kernel/include/ch.h chconf.h \
+ ../../ChibiOS-RT/os/ports/GCC/ARMCMx/chtypes.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.2/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.2/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.2/include/stdbool.h \
+ ../../ChibiOS-RT/os/kernel/include/chlists.h \
+ ../../ChibiOS-RT/os/ports/GCC/ARMCMx/chcore.h \
+ ../../ChibiOS-RT/os/ports/GCC/ARMCMx/STM32F4xx/cmparams.h \
+ ../../ChibiOS-RT/os/ports/GCC/ARMCMx/chcore_v7m.h \
+ ../../ChibiOS-RT/os/ports/common/ARMCMx/nvic.h \
+ ../../ChibiOS-RT/os/kernel/include/chsys.h \
+ ../../ChibiOS-RT/os/kernel/include/chvt.h \
+ ../../ChibiOS-RT/os/kernel/include/chschd.h \
+ ../../ChibiOS-RT/os/kernel/include/chsem.h \
+ ../../ChibiOS-RT/os/kernel/include/chbsem.h \
+ ../../ChibiOS-RT/os/kernel/include/chmtx.h \
+ ../../ChibiOS-RT/os/kernel/include/chcond.h \
+ ../../ChibiOS-RT/os/kernel/include/chevents.h \
+ ../../ChibiOS-RT/os/kernel/include/chmsg.h \
+ ../../ChibiOS-RT/os/kernel/include/chmboxes.h \
+ ../../ChibiOS-RT/os/kernel/include/chmemcore.h \
+ ../../ChibiOS-RT/os/kernel/include/chheap.h \
+ ../../ChibiOS-RT/os/kernel/include/chmempools.h \
+ ../../ChibiOS-RT/os/kernel/include/chthreads.h \
+ ../../ChibiOS-RT/os/kernel/include/chdynamic.h \
+ ../../ChibiOS-RT/os/kernel/include/chregistry.h \
+ ../../ChibiOS-RT/os/kernel/include/chinline.h \
+ ../../ChibiOS-RT/os/kernel/include/chqueues.h \
+ ../../ChibiOS-RT/os/kernel/include/chstreams.h \
+ ../../ChibiOS-RT/os/kernel/include/chfiles.h \
+ ../../ChibiOS-RT/os/kernel/include/chdebug.h \
+ ../../ChibiOS-RT/os/hal/include/hal.h \
+ ../boards/WAVESHARE_CORE407I/board.h halconf.h mcuconf.h \
+ ../../ChibiOS-RT/os/hal/platforms/STM32F4xx/hal_lld.h \
+ ../../ChibiOS-RT/os/hal/platforms/STM32/stm32.h \
+ ../../ChibiOS-RT/os/hal/platforms/STM32F4xx/stm32f4xx.h \
+ ../../ChibiOS-RT/os/ports/common/ARMCMx/CMSIS/include/core_cm4.h \
+ ../../ChibiOS-RT/os/ports/common/ARMCMx/CMSIS/include/core_cmInstr.h \
+ ../../ChibiOS-RT/os/ports/common/ARMCMx/CMSIS/include/core_cmFunc.h \
+ ../../ChibiOS-RT/os/ports/common/ARMCMx/CMSIS/include/core_cm4_simd.h \
+ ../../ChibiOS-RT/os/hal/platforms/STM32F4xx/stm32_isr.h \
+ ../../ChibiOS-RT/os/hal/platforms/STM32F4xx/stm32_dma.h \
+ ../../ChibiOS-RT/os/hal/platforms/STM32F4xx/stm32_rcc.h \
+ ../../ChibiOS-RT/os/hal/include/io_channel.h \
+ ../../ChibiOS-RT/os/hal/include/io_block.h \
+ ../../ChibiOS-RT/os/hal/include/mmcsd.h \
+ ../../ChibiOS-RT/os/hal/include/tm.h \
+ ../../ChibiOS-RT/os/hal/include/pal.h \
+ ../../ChibiOS-RT/os/hal/platforms/STM32/GPIOv2/pal_lld.h \
+ ../../ChibiOS-RT/os/hal/include/adc.h \
+ ../../ChibiOS-RT/os/hal/include/can.h \
+ ../../ChibiOS-RT/os/hal/platforms/STM32/can_lld.h \
+ ../../ChibiOS-RT/os/hal/include/ext.h \
+ ../../ChibiOS-RT/os/hal/include/gpt.h \
+ ../../ChibiOS-RT/os/hal/include/i2c.h \
+ ../../ChibiOS-RT/os/hal/include/icu.h \
+ ../../ChibiOS-RT/os/hal/include/mac.h \
+ ../../ChibiOS-RT/os/hal/include/pwm.h \
+ ../../ChibiOS-RT/os/hal/include/rtc.h \
+ ../../ChibiOS-RT/os/hal/include/serial.h \
+ ../../ChibiOS-RT/os/hal/include/sdc.h \
+ ../../ChibiOS-RT/os/hal/include/spi.h \
+ ../../ChibiOS-RT/os/hal/include/uart.h \
+ ../../ChibiOS-RT/os/hal/include/usb.h \
+ ../../ChibiOS-RT/os/hal/include/mmc_spi.h \
+ ../../ChibiOS-RT/os/hal/include/serial_usb.h
+
+../../ChibiOS-RT/os/kernel/include/ch.h:
+
+chconf.h:
+
+../../ChibiOS-RT/os/ports/GCC/ARMCMx/chtypes.h:
+
+/usr/lib/gcc/arm-none-eabi/4.9.2/include/stddef.h:
+
+/usr/lib/gcc/arm-none-eabi/4.9.2/include/stdint.h:
+
+/usr/arm-none-eabi/include/stdint.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
+
+/usr/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/lib/gcc/arm-none-eabi/4.9.2/include/stdbool.h:
+
+../../ChibiOS-RT/os/kernel/include/chlists.h:
+
+../../ChibiOS-RT/os/ports/GCC/ARMCMx/chcore.h:
+
+../../ChibiOS-RT/os/ports/GCC/ARMCMx/STM32F4xx/cmparams.h:
+
+../../ChibiOS-RT/os/ports/GCC/ARMCMx/chcore_v7m.h:
+
+../../ChibiOS-RT/os/ports/common/ARMCMx/nvic.h:
+
+../../ChibiOS-RT/os/kernel/include/chsys.h:
+
+../../ChibiOS-RT/os/kernel/include/chvt.h:
+
+../../ChibiOS-RT/os/kernel/include/chschd.h:
+
+../../ChibiOS-RT/os/kernel/include/chsem.h:
+
+../../ChibiOS-RT/os/kernel/include/chbsem.h:
+
+../../ChibiOS-RT/os/kernel/include/chmtx.h:
+
+../../ChibiOS-RT/os/kernel/include/chcond.h:
+
+../../ChibiOS-RT/os/kernel/include/chevents.h:
+
+../../ChibiOS-RT/os/kernel/include/chmsg.h:
+
+../../ChibiOS-RT/os/kernel/include/chmboxes.h:
+
+../../ChibiOS-RT/os/kernel/include/chmemcore.h:
+
+../../ChibiOS-RT/os/kernel/include/chheap.h:
+
+../../ChibiOS-RT/os/kernel/include/chmempools.h:
+
+../../ChibiOS-RT/os/kernel/include/chthreads.h:
+
+../../ChibiOS-RT/os/kernel/include/chdynamic.h:
+
+../../ChibiOS-RT/os/kernel/include/chregistry.h:
+
+../../ChibiOS-RT/os/kernel/include/chinline.h:
+
+../../ChibiOS-RT/os/kernel/include/chqueues.h:
+
+../../ChibiOS-RT/os/kernel/include/chstreams.h:
+
+../../ChibiOS-RT/os/kernel/include/chfiles.h:
+
+../../ChibiOS-RT/os/kernel/include/chdebug.h:
+
+../../ChibiOS-RT/os/hal/include/hal.h:
+
+../boards/WAVESHARE_CORE407I/board.h:
+
+halconf.h:
+
+mcuconf.h:
+
+../../ChibiOS-RT/os/hal/platforms/STM32F4xx/hal_lld.h:
+
+../../ChibiOS-RT/os/hal/platforms/STM32/stm32.h:
+
+../../ChibiOS-RT/os/hal/platforms/STM32F4xx/stm32f4xx.h:
+
+../../ChibiOS-RT/os/ports/common/ARMCMx/CMSIS/include/core_cm4.h:
+
+../../ChibiOS-RT/os/ports/common/ARMCMx/CMSIS/include/core_cmInstr.h:
+
+../../ChibiOS-RT/os/ports/common/ARMCMx/CMSIS/include/core_cmFunc.h:
+
+../../ChibiOS-RT/os/ports/common/ARMCMx/CMSIS/include/core_cm4_simd.h:
+
+../../ChibiOS-RT/os/hal/platforms/STM32F4xx/stm32_isr.h:
+
+../../ChibiOS-RT/os/hal/platforms/STM32F4xx/stm32_dma.h:
+
+../../ChibiOS-RT/os/hal/platforms/STM32F4xx/stm32_rcc.h:
+
+../../ChibiOS-RT/os/hal/include/io_channel.h:
+
+../../ChibiOS-RT/os/hal/include/io_block.h:
+
+../../ChibiOS-RT/os/hal/include/mmcsd.h:
+
+../../ChibiOS-RT/os/hal/include/tm.h:
+
+../../ChibiOS-RT/os/hal/include/pal.h:
+
+../../ChibiOS-RT/os/hal/platforms/STM32/GPIOv2/pal_lld.h:
+
+../../ChibiOS-RT/os/hal/include/adc.h:
+
+../../ChibiOS-RT/os/hal/include/can.h:
+
+../../ChibiOS-RT/os/hal/platforms/STM32/can_lld.h:
+
+../../ChibiOS-RT/os/hal/include/ext.h:
+
+../../ChibiOS-RT/os/hal/include/gpt.h:
+
+../../ChibiOS-RT/os/hal/include/i2c.h:
+
+../../ChibiOS-RT/os/hal/include/icu.h:
+
+../../ChibiOS-RT/os/hal/include/mac.h:
+
+../../ChibiOS-RT/os/hal/include/pwm.h:
+
+../../ChibiOS-RT/os/hal/include/rtc.h:
+
+../../ChibiOS-RT/os/hal/include/serial.h:
+
+../../ChibiOS-RT/os/hal/include/sdc.h:
+
+../../ChibiOS-RT/os/hal/include/spi.h:
+
+../../ChibiOS-RT/os/hal/include/uart.h:
+
+../../ChibiOS-RT/os/hal/include/usb.h:
+
+../../ChibiOS-RT/os/hal/include/mmc_spi.h:
+
+../../ChibiOS-RT/os/hal/include/serial_usb.h:
