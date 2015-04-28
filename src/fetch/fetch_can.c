bb40ef1e45f87b325715e3c8279a782a8b196626
SethMWard <setward@pdx.edu>
2015-04-27 23:24:49 -0700

    Edited fetch_can.c and updated io_manage_defs.h with available allocation for IO_CAN to pins B6, B5, I9, H13

--- src/fetch/fetch_can.c
+++ src/fetch/fetch_can.c
@@ -36,7 +36,7 @@ static fetch_command_t fetch_can_commands[] = {
 
    
     { fetch_can_config_cmd,    "config",    "Configure CAN driver\n" \
-                                            "Usage: config(<dev>)" },
+                                            "Usage: config(<dev>,<DLC>,<data32[2]>)" },
     { fetch_can_reset_cmd,     "reset",     "Reset CAN driver\n" \
                                             "Usage: reset(<dev>)" },
     { fetch_can_help_cmd,      "help",      "CAN command help" },
@@ -78,7 +78,7 @@ static CANDriver * parse_can_dev(char * str, int32_t * dev)
  */
 static const CANConfig can_cfg = {
   CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
-  CAN_BTR_LBKM | CAN_BTR_SJW(0) | CAN_BTR_TS2(1) |
+  CAN_BTR_SJW(0) | CAN_BTR_TS2(1) |
   CAN_BTR_TS1(8) | CAN_BTR_BRP(6)
 };
 
@@ -86,7 +86,7 @@ void fetch_can_init(BaseSequentialStream * chp)
 {
  /*gpio_heartbeat_thread = chThdCreateStatic(wa_gpio_heartbeat_thread, sizeof(wa_gpio_heartbeat_thread), NORMALPRIO, (tfunc_t)gpio_heartbeat_tfunc, NULL);*/
 
-  can_init_flag = false;
+  can_init_flag = false; /*Not sure what this is doing or if it is needed*/
 }
 
 /*! \brief dispatch a specific gpio command
@@ -114,7 +114,7 @@ static bool fetch_can_config_cmd(BaseSequentialStream * chp, char * cmd_list[],c
  int32_t can_dev; /*Can Device */
  CANDriver * can_drv; /*Can driver */
  
- if(!fetch_input_check(chp, cmd_list, FETCH_TOK_SUBCMD_0, data_list, 7)) //max data field is temporary till I figure this out
+ if(!fetch_input_check(chp, cmd_list, FETCH_TOK_SUBCMD_0, data_list, 3)) //max data field is temporary till I figure this out
  {
   return false;
  }
@@ -126,8 +126,8 @@ static bool fetch_can_config_cmd(BaseSequentialStream * chp, char * cmd_list[],c
  switch( can_dev )
  {
     case 1:
-      if( !io_manage_set_mode( can1_pins[0].port, can1_pins[0].pin, PAL_MODE_ALTERNATE(4) | PAL_STM32_OTYPE_OPENDRAIN, IO_CAN) ||
-          !io_manage_set_mode( can1_pins[1].port, can1_pins[1].pin, PAL_MODE_ALTERNATE(4) | PAL_STM32_OTYPE_OPENDRAIN, IO_CAN) )
+      if( !io_manage_set_mode( can1_pins[0].port, can1_pins[0].pin, PAL_MODE_ALTERNATE(9) | PAL_STM32_OTYPE_OPENDRAIN, IO_CAN) ||
+          !io_manage_set_mode( can1_pins[1].port, can1_pins[1].pin, PAL_MODE_ALTERNATE(9) | PAL_STM32_OTYPE_OPENDRAIN, IO_CAN) )
       {
         util_message_error(chp, "unable to allocate pins");
         io_manage_set_default_mode( can1_pins[0].port, can1_pins[0].pin, IO_CAN );
@@ -136,8 +136,8 @@ static bool fetch_can_config_cmd(BaseSequentialStream * chp, char * cmd_list[],c
       }
       break;
     case 2:
-      if( !io_manage_set_mode( can2_pins[0].port, can2_pins[0].pin, PAL_MODE_ALTERNATE(4) | PAL_STM32_OTYPE_OPENDRAIN, IO_CAN) ||
-          !io_manage_set_mode( can2_pins[1].port, can2_pins[1].pin, PAL_MODE_ALTERNATE(4) | PAL_STM32_OTYPE_OPENDRAIN, IO_CAN) )
+      if( !io_manage_set_mode( can2_pins[0].port, can2_pins[0].pin, PAL_MODE_ALTERNATE(9) | PAL_STM32_OTYPE_OPENDRAIN, IO_CAN) ||
+          !io_manage_set_mode( can2_pins[1].port, can2_pins[1].pin, PAL_MODE_ALTERNATE(9) | PAL_STM32_OTYPE_OPENDRAIN, IO_CAN) )
       {        util_message_error(chp, "unable to allocate pins");
         io_manage_set_default_mode( can2_pins[0].port, can2_pins[0].pin, IO_CAN );
         io_manage_set_default_mode( can2_pins[1].port, can2_pins[1].pin, IO_CAN );
@@ -146,13 +146,12 @@ static bool fetch_can_config_cmd(BaseSequentialStream * chp, char * cmd_list[],c
       break;
  }
 
- /*#if STM32_CAN_USE_CAN1
+ #if STM32_CAN_USE_CAN1
   canStart(&CAND1, &can_cfg);
  #endif
  #if STM32_CAN_USE_CAN2
   canStart(&CAND2, &can_cfg);
- #endif*/
- canStart(can_drv, &can_cfg);
+ #endif
  return true;
 }
 
